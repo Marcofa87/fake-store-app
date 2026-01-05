5843b06571726c52603244bcb909a0b13abf2e9f
marco <mfalasca@tecnicalservice.net>
2026-01-05 14:18:29 +0100

    test api id

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -17,9 +17,6 @@ export async function GET() {
     const products = await response.json();
     return NextResponse.json(products);
   } catch (error) {
-    return NextResponse.json(
-      { error: (error as Error).message },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: error }, { status: 500 });
   }
 }
