b2911f5930bb3a7a623d13738b363a7ff40251ee
marco <mfalasca@tecnicalservice.net>
2026-01-05 17:26:07 +0100

    installata libreria cn, shadcn e implementata card del prodotto nel dettaglio

--- app/(dashboard)/products/[id]/page.tsx
+++ app/(dashboard)/products/[id]/page.tsx
@@ -1,6 +1,23 @@
 import { notFound } from "next/navigation";
+import Image from "next/image";
+
+import { HeartIcon } from "lucide-react";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardDescription,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+
+import { cn } from "@/lib/utils";
 
 type Product = {
+  images: string[];
   id: number;
   title: string;
   description: string;
@@ -32,8 +49,57 @@ export default async function ProductsIdPage({
 }: {
   params: Promise<{ id: string }>;
 }) {
+  /*  const [liked, setLiked] = useState<boolean>(false); */
   const { id } = await params;
   const product = await getProductsId(id);
 
-  return <div>{product.title}</div>;
+  return (
+    <div className="relative max-w-md rounded-xl bg-gradient-to-r from-neutral-600 to-violet-300 pt-0 shadow-lg">
+      <div className="flex h-60 items-center justify-center">
+        <Image
+          src={product.images?.[0] || ""}
+          alt={product.title}
+          width={300}
+          height={300}
+          className="w-75"
+        />
+      </div>
+      <Button
+        size="icon"
+        /* onClick={() => setLiked(!liked)} */
+        className="bg-primary/10 hover:bg-primary/20 absolute top-4 right-4 rounded-full"
+      >
+        <HeartIcon
+          className={cn(
+            /* liked ? "fill-destructive stroke-destructive" : "stroke-white" */
+            "stroke-white"
+          )}
+        />
+        <span className="sr-only">Like</span>
+      </Button>
+      <Card className="border-none">
+        <CardHeader>
+          <CardTitle>{product.title}</CardTitle>
+          <CardDescription className="flex items-center gap-2">
+            <Badge variant="outline" className="rounded-sm">
+              {product.category.name}
+            </Badge>
+            <Badge variant="outline" className="rounded-sm">
+              {product.category.name}
+            </Badge>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>{product.description}</p>
+        </CardContent>
+        <CardFooter className="justify-between gap-3 max-sm:flex-col max-sm:items-stretch">
+          <div className="flex flex-col">
+            <span className="text-sm font-medium uppercase">Price</span>
+            <span className="text-xl font-semibold">${product.price}</span>
+          </div>
+          <Button size="lg">Add to cart</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
