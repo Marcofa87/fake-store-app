6b9b0b9907a14b42b25bf61e8634cce8224e8953
marco <mfalasca@tecnicalservice.net>
2026-01-05 10:43:04 +0100

    centrato layout login

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col items-center justify-center ">{children}</div>
+    <div className="flex flex-col items-center justify-center  h-screen">
+      {children}
+    </div>
   );
 }
 
