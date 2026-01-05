f5684528bcc8897b9ea8559b69dfe558edf44359
marco <mfalasca@tecnicalservice.net>
2026-01-05 12:10:20 +0100

    modificato layout dei prodotti

--- app/(dashboard)/products/page.tsx
+++ app/(dashboard)/products/page.tsx
@@ -28,21 +28,21 @@ export default async function ProductsPage() {
   console.log(products);
 
   return (
-    <div className="p-8">
-      <h1>Products</h1>
-      <div className="grid grid-cols-8 gap-8">
+    <div className="p-4 sm:p-6 lg:p-8">
+      <h1 className="mb-6">Products</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-8">
         {products.map((product: Product) => (
           <div
             key={product.id}
-            className="border-2 border-gray-300 rounded-md p-4"
+            className="border-2 border-gray-300 rounded-md p-4 sm:p-6"
           >
             <img
               src={product.category.image}
               alt={product.title}
-              className="w-full h-48 object-cover"
+              className="w-full h-48 object-cover mb-4"
             />
-            <h2 className="text-lg font-bold">{product.title}</h2>
-            <p className="text-sm text-gray-500">{product.description}</p>
+            <h2 className="text-lg font-bold mb-2">{product.title}</h2>
+            <p className="text-sm text-gray-500 mb-2">{product.description}</p>
             <p className="text-sm text-gray-500">{product.price}</p>
           </div>
         ))}
